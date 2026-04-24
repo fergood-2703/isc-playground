fa305c32ca77947f5ad8d5b66ef9aa2b74b147c7
FerGood <omi.omar.2703@gmail.com>
2026-04-23 19:28:26 -0500

    Modificando y agregando carpetas de images para el navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import "./Navbar.css";
+import logo from "../../assets/images/logo-isc.png";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <h2>🎮 ISC Playground</h2>
 
+      {/* IZQUIERDA */}
+      <div className="nav-left">
+        <img src={logo} alt="ISC" className="logo" />
+        <span className="brand">ISC Playground</span>
+      </div>
+
+      {/* DERECHA */}
       <div className="nav-links">
         <a href="#">Inicio</a>
         <a href="#">Juegos</a>
+        <a href="#">About Us</a>
         <button className="login-btn">Login</button>
       </div>
+
     </div>
   );
 }
