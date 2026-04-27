bd26f18b8f7d34852e3aaf6edaa435eead9062e7
FerGood <omi.omar.2703@gmail.com>
2026-04-27 12:08:37 -0500

    Agregando la vista de juegos en el dashboard de administrador

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Registro from "./pages/Registro/Registro";
-import DashboardLayout from "./pages/Dashboard/DashboardLayout"; // 👈 corregido
+import DashboardLayout from "./pages/Dashboard/DashboardLayout"; // 
+import Juegos from "./pages/Dashboard/views/Juegos";
 
 export default function App() {
   return (
@@ -18,7 +19,7 @@ export default function App() {
         {/* ADMIN DASHBOARD */}
         <Route path="/admin" element={<DashboardLayout />}>
           <Route index element={<h2>Dashboard Home</h2>} />
-          <Route path="juegos" element={<h2>Juegos</h2>} />
+          <Route path="juegos" element={<Juegos/>} />
           <Route path="usuarios" element={<h2>Usuarios</h2>} />
           <Route path="equipos" element={<h2>Equipos</h2>} />
           <Route path="ranking" element={<h2>Ranking</h2>} />
