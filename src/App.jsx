d8e1787e5cca4b545a31bf87453c8eef77b00415
Mike <alonzoomichael@gmail.com>
2026-04-27 16:37:38 -0500

    Subiendo Vista de detalles de los juegos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Registro from "./pages/Registro/Registro";
+import Detalles from "./pages/Juegos/Detalles/Detalles";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/juego/:id" element={<Detalles />} />
       </Routes>
     </BrowserRouter>
   );
